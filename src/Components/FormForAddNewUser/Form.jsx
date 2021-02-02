c1b900234e6fdbb712f5661e7e56fd75195774b2
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2021-02-02 12:34:59 +0200

    fix bug in form

--- src/Components/FormForAddNewUser/Form.jsx
+++ src/Components/FormForAddNewUser/Form.jsx
@@ -9,20 +9,24 @@ export const Form = () => (
     <form className="new-employee-form" name="addUser" method="get">
       <label className="labelForm" htmlFor="name">
         Full name:*
-        <input className="inputForm" type="text" name="name" id="name" required placeholder="Irina Sokolova" />
+        {}
       </label>
+      <input className="inputForm" type="text" name="name" id="name" required placeholder="Irina Sokolova" />
       <label className="labelForm" htmlFor="email">
         Email:*
-        <input className="inputForm" type="email" name="email" id="email" required placeholder="ivanov@gmail.com" />
+        {}
       </label>
+      <input className="inputForm" type="email" name="email" id="email" required placeholder="ivanov@gmail.com" />
       <label className="labelForm" htmlFor="phone">
         Phone:*
-        <input className="inputForm" type="tel" name="phone" id="phone" required placeholder="+38(097)532-23-11" />
+        {}
       </label>
+      <input className="inputForm" type="tel" name="phone" id="phone" required placeholder="+38(097)532-23-11" />
       <label className="labelForm" htmlFor="webSite">
         Web site:*
-        <input className="inputForm" type="url" name="webSite" id="webSite" required placeholder="https://mysite.com" />
+        {}
       </label>
+      <input className="inputForm" type="url" name="webSite" id="webSite" required placeholder="https://mysite.com" />
       <button
         type="submit"
         className="formButton"
